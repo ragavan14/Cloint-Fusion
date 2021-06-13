b92fba19971d432f418b193ae876816a2c3138f4
ragavan14 <ragavanboopathi1999@gmail.com>
2021-06-13 20:57:45 +0530

    Add files via upload

---
+++ Hackathon task/Hackathon 6.0.py
@@ -0,0 +1,15 @@
+import ClointFusion as cf
+cf.launch_website_h("https://www.instagram.com/?hl=en")
+cf.browser_write_h("ragavan_6005",User_Visible_Text_Element="Phone number, username, or email")
+cf.browser_write_h("rasow123#",User_Visible_Text_Element="Password")
+cf.browser_mouse_click_h("Log In")
+cf.mouse_click()
+680,700
+cf.mouse_click()
+365,410
+ cf.mouse_click(560,210,single_double_triple='double')
+cf.browser_mouse_click_h("Next")
+cf.mouse_click()
+300,200
+cf.browser_write_h("Elon Musk's most awaited one",User_Visible_Text_Element="Write a caption...")
+cf.browser_mouse_click_h("Share")
